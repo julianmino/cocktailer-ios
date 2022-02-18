5e5c996351c329f82f83a399000b260af126a5b8
Julián Miño <m99_julian@hotmail.com>
2022-02-18 14:17:43 -0300

    MODIFY improve alertHelper

--- cocktailer-ios/cocktailer-ios/Helpers/AlertHelper.swift
+++ cocktailer-ios/cocktailer-ios/Helpers/AlertHelper.swift
@@ -13,7 +13,7 @@ class AlertHelper {
     static func showErrorAlert(message: String) {
         let alertView = UIAlertController(title: R.string.localizable.error(), message: message, preferredStyle: .alert)
         alertView.addAction(UIAlertAction(title: R.string.localizable.ok(), style: .default, handler: nil))
-        guard let rootVC = UIApplication.shared.windows.first(where: {$0.isKeyWindow})?.rootViewController else {return}
+        guard let rootVC = UIApplication.shared.keyWindow?.rootViewController else {return}
         if let presentedVC = rootVC.presentedViewController {
             DispatchQueue.main.async {
                 presentedVC.present(alertView, animated: true, completion: nil)

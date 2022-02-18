b290fd127d4591fb8dc11170b0b5d2e946f09082
Julián Miño <m99_julian@hotmail.com>
2022-02-18 14:17:10 -0300

    FIX Info.plist & AppDelegate

--- cocktailer-ios/cocktailer-ios/Supporting Files/AppDelegate.swift
+++ cocktailer-ios/cocktailer-ios/Supporting Files/AppDelegate.swift
@@ -9,17 +9,12 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-                window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
-                window?.makeKeyAndVisible()
-
-                return true
         return true
     }
-
+    
 }
 

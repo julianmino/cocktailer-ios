42d218e0fde77bed58faf4f50befec78508b8518
Julián Miño <m99_julian@hotmail.com>
2022-02-18 11:26:25 -0300

    ADD install pods
    Note: for convenience purposes upon trying out the app, I preferred not to ignore the Pods folder.

---
+++ cocktailer-ios/Pods/Target Support Files/Pods-cocktailer-ios-cocktailer-iosUITests/Pods-cocktailer-ios-cocktailer-iosUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cocktailer_ios_cocktailer_iosUITests : NSObject
+@end
+@implementation PodsDummy_Pods_cocktailer_ios_cocktailer_iosUITests
+@end

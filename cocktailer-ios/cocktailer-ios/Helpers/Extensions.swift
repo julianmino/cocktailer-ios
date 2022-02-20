308a57f81c2c64664fb0ce04b494d00fca2654ee
Julián Miño <m99_julian@hotmail.com>
2022-02-20 03:59:06 -0300

    ADD UIColor extensions

---
+++ cocktailer-ios/cocktailer-ios/Helpers/Extensions.swift
@@ -0,0 +1,34 @@
+//
+//  Extensions.swift
+//  cocktailer-ios
+//
+//  Created by Julian Miño on 20/02/2022.
+//
+
+import Foundation
+import UIKit
+import Kingfisher
+
+extension UIColor {
+    
+    private static var cocktailerPrimary: UIColor {
+        return UIColor(red: 225/255, green: 73/255, blue: 0, alpha: 1)
+    }
+    
+    private static var cocktailerSecondary: UIColor {
+        return UIColor(red: 133/255, green: 146/255, blue: 166/255, alpha: 1)
+    }
+    
+    private static var cocktailerTertiary: UIColor {
+        return UIColor(red: 186/255, green: 60/255, blue: 81/255, alpha: 1)
+    }
+    
+    private static var cocktailerLightColor: UIColor {
+        return UIColor(red: 243/255, green: 248/255, blue: 243/255, alpha: 1)
+    }
+    
+    private static var cocktailerDarkColor: UIColor {
+        return UIColor(red: 31/255, green: 20/255, blue: 31/255, alpha: 1)
+    }
+}
+

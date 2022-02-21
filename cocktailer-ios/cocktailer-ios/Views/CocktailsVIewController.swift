5c4f217dad27ff5cc89067e7c5b45b651582e357
Julián Miño <m99_julian@hotmail.com>
2022-02-21 01:47:29 -0300

    MODIFY CocktailsVC view setup

--- cocktailer-ios/cocktailer-ios/Views/CocktailsVIewController.swift
+++ cocktailer-ios/cocktailer-ios/Views/CocktailsVIewController.swift
@@ -20,10 +20,39 @@ class CocktailsViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter.attach(self)
+        setup()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+    }
+    
+    private func setup() {
+        view.backgroundColor = .cocktailerLightColor
+        titleLabel.set(
+            font: UIFont.systemFont(ofSize: 30, weight: .bold),
+            color: .cocktailerPrimary,
+            align: .center)
+        descriptionLabel.set(
+            font: UIFont.systemFont(ofSize: 18, weight: .semibold),
+            color: .cocktailerDarkColor,
+            align: .center)
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.adjustsFontSizeToFitWidth = true
+        
+        titleLabel.text = R.string.localizable.cocktailer()
+        imageView.image = R.image.cocktailerIcon()
+        descriptionLabel.text = R.string.localizable.searchForADrinkMessage()
+        
+        searchButton.layoutIfNeeded()
+        searchTextField.returnKeyType = .done
+        searchTextField.delegate = self
+        searchButton.roundBorders(searchButton.frame.height / 2)
+        searchButton.set(
+            title: R.string.localizable.search(),
+            image: nil,
+            tintColor: .cocktailerLightColor,
+            bgColor: .cocktailerPrimary)
     }
 }
 
